b5086ae4e5fa543fbd1d3ddf49841e923c662d51
LuanLuu <lluu3@mail.sfsu.edu>
2021-01-15 12:58:11 -0800

    Modify apiServer port

--- apiServer.js
+++ apiServer.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const     app = express();
-const    PORT = 4000;
+const    PORT = 4001;
 
 app.get('/api/data', (req, res) => {
     res.send('api data received');
