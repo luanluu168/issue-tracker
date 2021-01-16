a068ac78102f8d06dd832795f0cd8bf0e71b48cb
LuanLuu <lluu3@mail.sfsu.edu>
2021-01-16 14:50:10 -0800

    Adjust dropdown menu for loggedin condition

--- public/javascripts/javascripts.js
+++ public/javascripts/javascripts.js
@@ -13,24 +13,28 @@ const outCallback = () => { // the case where the dropdown menu is not hovered
   dropdownToggle.attr("aria-expanded", "false");
   dropdownMenu.removeClass("show");
 };
+const getDropdownComponents = () => {
+  dropdown       = $(".dropdown");
+  dropdownToggle = $(".dropdown-toggle");
+  dropdownMenu   = $(".dropdown-menu");
+}
 const modifyDropdownProps = () => {
+  // if the window size is greater than MIN_WIDTH then add some new classes & attrs for the dropdown components
   if (window.matchMedia("(min-width: " + MIN_WIDTH + "px)").matches) {
-    if (dropdown === undefined || dropdownToggle === undefined || dropdownMenu === undefined) return;
+    // check if components fail to load
+    if (dropdown === undefined || dropdownToggle === undefined || dropdownMenu === undefined) getDropdownComponents();
     
     dropdown.hover(inCallback, outCallback);
     return;
   } 
-
+  // otherwise, remove the above added classes & attrs for the dropdown components
   dropdown.off("mouseenter mouseleave");
 };
 window.onresize = () => { 
   modifyDropdownProps();
 }; 
 window.onload = () => {
-  dropdown       = $(".dropdown");
-  dropdownToggle = $(".dropdown-toggle");
-  dropdownMenu   = $(".dropdown-menu");
-
+  getDropdownComponents();
   modifyDropdownProps();  
 };
 // ------- end dropdown menu hover ---------
