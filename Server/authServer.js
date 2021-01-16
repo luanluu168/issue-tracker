cacbc0fa9983a247aae897cbdf8b666fcd1c9512
LuanLuu <lluu3@mail.sfsu.edu>
2021-01-16 00:03:31 -0800

    Dynamic year added in authServer

--- Server/authServer.js
+++ Server/authServer.js
@@ -4,8 +4,11 @@ const    PORT = 4002;
 
 app.set('view engine', 'pug');
 
+let       today = new Date();
+let currentYear = today ? today.getFullYear() : 2020;
+
 app.get('/auth/server', (req, res) => {
-    res.render('auth/signin', {success: true});
+    res.render('auth/signin', { year: currentYear });
 });
 
 app.listen(PORT, () => console.log(`authServer is listening on port ${PORT}`));
