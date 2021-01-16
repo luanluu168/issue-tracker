bf72d228c0bf1f254beed074c16ba599d38b2692
LuanLuu <lluu3@mail.sfsu.edu>
2021-01-16 15:57:47 -0800

    Signup page and route added

--- Server/authServer.js
+++ Server/authServer.js
@@ -7,8 +7,12 @@ app.set('view engine', 'pug');
 let       today = new Date();
 let currentYear = today ? today.getFullYear() : 2020;
 
-app.get('/auth/server', (req, res) => {
-    res.render('auth/signin', { year: currentYear });
+app.get('/auth/server/signin', (req, res) => {
+    res.render('auth/signin', { year: currentYear, actionType: 'signin', error: '' });
+});
+
+app.get('/auth/server/signup', (req, res) => {
+    res.render('auth/signup', { year: currentYear, actionType: 'signup', error: ''});
 });
 
 app.listen(PORT, () => console.log(`authServer is listening on port ${PORT}`));
