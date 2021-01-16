fdaa7e66519d3669db322ead5794d576093c92d5
LuanLuu <lluu3@mail.sfsu.edu>
2021-01-16 14:46:22 -0800

    Fix javascript

--- public/javascripts/javascripts.js
+++ public/javascripts/javascripts.js
@@ -5,17 +5,16 @@ let dropdownToggle = undefined;
 let dropdownMenu   = undefined;
 const inCallback   = () => { // the case where the dropdown menu is hovered
   dropdown.addClass("show");
-  dropdown.find(dropdownToggle).attr("aria-expanded", "true");
-  dropdown.find(dropdownMenu).addClass("show");
+  dropdownToggle.attr("aria-expanded", "true");
+  dropdownMenu.addClass("show");
 };
 const outCallback = () => { // the case where the dropdown menu is not hovered
   dropdown.removeClass("show");
-  dropdown.find(dropdownToggle).attr("aria-expanded", "false");
-  dropdown.find(dropdownMenu).removeClass("show");
+  dropdownToggle.attr("aria-expanded", "false");
+  dropdownMenu.removeClass("show");
 };
 const modifyDropdownProps = () => {
   if (window.matchMedia("(min-width: " + MIN_WIDTH + "px)").matches) {
-    console.log(dropdown);
     if (dropdown === undefined || dropdownToggle === undefined || dropdownMenu === undefined) return;
     
     dropdown.hover(inCallback, outCallback);
