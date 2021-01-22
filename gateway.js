dc4c38de9afd6adf174ab50ebda63ec363e1dc53
LuanLuu <lluu3@mail.sfsu.edu>
2021-01-21 19:50:23 -0800

    Fix gateway to pass html form body from proxy to another service/server

--- gateway.js
+++ gateway.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const   express = require('express');
 const      path = require('path');
+const   express = require('express');
 const httpProxy = require('http-proxy');
 const    morgan = require('morgan');
 const      PORT = 4000;
@@ -10,11 +10,23 @@ const     proxy = httpProxy.createProxyServer();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+
 app.use(morgan('dev'));
+app.use(express.json()); // parse application/json 
+app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(express.static(path.join(__dirname, 'public')));
 
 proxy.on('error', (err, req, res) => {
     res.status(500).send(`Proxy error: ${err}`);
+});
+// need this to parse html form body elements' values from proxy to another service/server 
+proxy.on('proxyReq', (proxyReq, req) => {
+    if (req.body && req.complete) {
+      const bodyData = JSON.stringify(req.body);
+      proxyReq.setHeader('Content-Type', 'application/json');
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+      proxyReq.write(bodyData);
+    }
 });
 
 let       today = new Date();
