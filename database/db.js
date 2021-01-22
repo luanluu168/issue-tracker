7ffd9fceccc7687bd026f3876f6a4ab96508c61e
LuanLuu <lluu3@mail.sfsu.edu>
2021-01-22 11:44:32 -0800

    Fix pg_hba ssl error

--- database/db.js
+++ database/db.js
@@ -1,4 +1,9 @@
-const    pgPromise = require('pg-promise')({});
-const dbConnection =  pgPromise(process.env.DATABASE_URL);
+require('dotenv').config();
+const config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false } // need to set this for production to resolve error pg_hba ssl
+};
+const    pgPromise = require('pg-promise')();
+const dbConnection = pgPromise(config);
 
 module.exports = dbConnection;
