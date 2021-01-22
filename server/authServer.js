bd4d62d43a5d45b71ea86ee93c97969c554553be
LuanLuu <lluu3@mail.sfsu.edu>
2021-01-21 20:32:49 -0800

    Clean up pyramid signin code in authServer

--- server/authServer.js
+++ server/authServer.js
@@ -61,62 +61,18 @@ app.get('/auth/server/signin', (req, res) => {
     res.render('auth/signin', { year: currentYear, actionType: 'signin', error: '' });
 });
 app.post('/auth/server/signin/query', (req, res) => {
-    console.log('testing is called');
     let    loginEmail = `'${req.body.userEmail}'`;
     let loginPassword = `'${req.body.userPassword}'`;
     let query = `SELECT id, email, password, role FROM "Users" WHERE email=${loginEmail}`;
     console.log(`query: ${query}`);
     let body = JSON.stringify(req.body);
-    console.log(`body: ${body}`);
-    // dbConnection.any(query)
-    //             .then((data) => {
-    //                 console.log(`Testing 1 pass...`);
-    //                 if (data.length === 0) {
-    //                     return res.json({error: 'Email was not correct'});
-    //                 }
-                    
-    //                 let tmp = JSON.stringify(data);
-    //                 console.log(`Testing 2 pass... ${tmp}`);
-    //                 const isValidatePassword = bcrypt.compareSync(loginPassword, data[0].password);
-    //                 console.log(`Testing 3 pass... ${tmp}`);
-    //                 let userPassword = `'${data[0].password}'`;
-    //                 console.log(`userRole == 'admin' ? ${data[0].role == 'admin'}`);
-    //                 console.log(`userPassword == loginPassword ? ${userPassword == loginPassword}`);
-    //                 // users can be created directly in PG or through the app that use bcrypt, so need to check 2 cases: with and without bcrypt
-    //                 if ((data[0].role == 'user' || data[0].role == 'admin') && (userPassword == loginPassword || isValidatePassword)) {
-    //                     console.log(`Testing 4 pass...`);
-    //                     req.session.valid = true;
-    //                     req.session.User = {
-    //                         aId:   data[0].id,
-    //                         role:  data[0].role,
-    //                         name:  data[0].customerName,
-    //                         email: loginEmail,
-    //                         isLoggedin: true
-    //                     };
-    //                     res.cookie("userLoginInfo", 
-    //                                 JSON.stringify(req.session.User), 
-    //                                 { maxAge: 2 * 60 * 60 * 100 });
-    //                     // res.status(200).json({ actionType: 'signin', status: 'success', error: 'None' });
-    //                     res.render('pages/home', { year: currentYear, actionType: 'signin', status: 'success', error: 'None' });
-    //                 } else {
-    //                     console.log(`Testing 5 pass...`);
-    //                     // res.json({ error: 'Password is wrong' });
-    //                     res.render('auth/signin', { year: currentYear, actionType: 'signin', status: 'fail', error: 'Password is wrong' });
-    //                 }
-    //             })
-    //             .catch( (e) => {
-    //                 console.log(`Error in authServer: ${e}`);
-    //                 res.json({ error: e });
-    //             }); 
 
     let promise = findUser(`email`, loginEmail);
-    console.log(`!!!!!!!!!!!!!! promise`);
     promise.then((data) => {
         let userPassword = `'${data.password}'`;
         const isValidatePassword = bcrypt.compareSync(loginPassword, userPassword);
         // users can be created directly in PG or through the app that use bcrypt, so need to check 2 cases: with and without bcrypt
         if ((data.role == 'user' || data.role == 'admin') && (userPassword == loginPassword || isValidatePassword)) {
-            console.log(`Testing 4 pass...`);
             req.session.valid = true;
             req.session.User = {
                 aId:   data.id,
@@ -128,17 +84,16 @@ app.post('/auth/server/signin/query', (req, res) => {
             res.cookie("userLoginInfo", 
                         JSON.stringify(req.session.User), 
                         { maxAge: 2 * 60 * 60 * 100 });
-            // res.status(200).json({ actionType: 'signin', status: 'success', error: 'None' });
             res.render('pages/home', { year: currentYear, actionType: 'signin', status: 'success', error: 'None' });
         } else {
-            console.log(`Testing 5 pass...`);
-            // res.json({ error: 'Password is wrong' });
             res.render('auth/signin', { year: currentYear, actionType: 'signin', status: 'fail', error: 'Password is wrong' });
         }
     })
     .catch( (e) => {
         console.log(`Error signin @ authServer: ${e}`);
-        res.json({ error: e });
+        if (e.code == '404')
+            return res.render('auth/signin', { year: currentYear, actionType: 'signin', status: e.status, error: 'Email is not existed' });
+            res.render('pages/error', { year: currentYear, actionType: 'signin', status: e.status, error: e });
     });
 });
 
