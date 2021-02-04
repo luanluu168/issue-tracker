ed6899708fb2c47339102c5018555118595091ac
LuanLuu <lluu3@mail.sfsu.edu>
2021-02-03 17:16:51 -0800

    Crud user function added

--- database/users.js
+++ database/users.js
@@ -65,7 +65,7 @@ const registerUserByOAuth = (user, bc, SALT_ROUNDS) => {
             if (err) { reject({ error: err, actionType: 'Hash Password', status: 'fail', code: '500'}); }
 
             const    userId = parseInt(("" + user.aId).substring(0,7));
-            const     query = `INSERT INTO "Users" (id, name, email, password, role) VALUES (${userId}, '${userName}', '${userEmail}', '${hashPass}', '${userRole}')`;
+            const     query = `INSERT INTO "Users" (id, name, email, password, role, is_social_account) VALUES (${userId}, '${userName}', '${userEmail}', '${hashPass}', '${userRole}', ${true})`;
             console.log(`------ registerUser query: ${query}`);
             dbConnection.any(query)
                         .then((data) => {
@@ -83,8 +83,90 @@ const registerUserByOAuth = (user, bc, SALT_ROUNDS) => {
     });
 };
 
+const setUserImage = (userEmail, imageFile, imageDir) => {
+    return new Promise((resolve, reject) => {
+                const query = `UPDATE "Users" SET image='${imageDir}${imageFile}' WHERE email='${userEmail}'`;
+                console.log(`query= ${query}`);
+                
+                dbConnection.any(query)
+                        .then((data) => {
+                            console.log(`Update user image successfully`);
+                            resolve({ error: 'Update user image successfully', actionType: 'Update user image', status: 'success', code: '200' });
+                        })
+                        .catch((e) => {
+                            console.log(`Fail to update user image, error= ${e}`);
+                            reject({ error: 'Fail to update user image', actionType: 'Update user image', status: 'fail', code: '400' });
+                        });
+    });
+};
+
+const updateUserNameAndPassword = (userName, userPassword, userEmail, bc, SALT_ROUNDS) => {
+    return new Promise((resolve, reject) => {
+        const callback = (err, hashPass) => {
+            if (err) { reject({ error: err, actionType: 'Hash Password', status: 'fail', code: '500'}); }
+
+            const query = `UPDATE "Users" SET name='${userName}', password='${hashPass}' WHERE email='${userEmail}'`;
+            console.log(`query= ${query}`);
+            dbConnection.any(query)
+                        .then((data) => {
+                            console.log(`Update user name and password successfully`);
+                            resolve({ error: 'Update user name and password successfully', actionType: 'Update user name and password', status: 'success', code: '200' });
+                        })
+                        .catch((e) => {
+                            console.log(`Fail to update user name, error= ${e}`);
+                            reject({ error: 'Fail to update user name', actionType: 'Update user name and password', status: 'fail', code: '400' });
+                        }); 
+        };
+        // use async bcrypt function to hash
+        bc.hash(userPassword, SALT_ROUNDS, callback);
+    });
+};
+
+const updateUserPassword = (userPassword, userEmail, bc, SALT_ROUNDS) => {
+    return new Promise((resolve, reject) => {
+        const callback = (err, hashPass) => {
+            if (err) { reject({ error: err, actionType: 'Hash Password', status: 'fail', code: '500'}); }
+
+            const query = `UPDATE "Users" SET password='${hashPass}' WHERE email='${userEmail}'`;
+            console.log(`query= ${query}`);
+            dbConnection.any(query)
+                        .then((data) => {
+                            console.log(`Update user password successfully`);
+                            resolve({ error: 'Update user password successfully', actionType: 'Update user password', status: 'success', code: '200' });
+                        })
+                        .catch((e) => {
+                            console.log(`Fail to update user password, error= ${e}`);
+                            reject({ error: 'Fail to update user password', actionType: 'Update user password', status: 'fail', code: '400' });
+                        }); 
+        };
+        // use async bcrypt function to hash
+        bc.hash(userPassword, SALT_ROUNDS, callback);
+    });
+};
+
+const updateUserName = (userName, userEmail) => {
+    return new Promise((resolve, reject) => {
+        const query = `UPDATE "Users" SET name='${userName}' WHERE email='${userEmail}'`;
+        console.log(`query= ${query}`);
+        
+        dbConnection.any(query)
+                .then((data) => {
+                    console.log(`Update user name successfully`);
+                    resolve({ error: 'Update user name successfully', actionType: 'Update user name', status: 'success', code: '200' });
+                })
+                .catch((e) => {
+                    console.log(`Fail to update user name, error= ${e}`);
+                    reject({ error: 'Fail to update user name', actionType: 'Update user name', status: 'fail', code: '400' });
+                });
+    });
+};
+
 module.exports = { 
     findUser,
     registerUser,
-    registerUserByOAuth
+    registerUserByOAuth,
+    setUserImage,
+    updateUserNameAndPassword,
+    updateUserPassword,
+    updateUserName
 };
