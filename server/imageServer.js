e19ff1db300c075265606cd8aa5f104a01274922
LuanLuu <lluu3@mail.sfsu.edu>
2021-02-04 16:03:59 -0800

    Fix image server error when no image is uploaded

--- server/imageServer.js
+++ server/imageServer.js
@@ -23,10 +23,9 @@ const fileFilter = (req, file, cb) => {
     }
 };
 const limits = {
-    files: 1, // 1 file per request
-    fileSize: 1024 * 1024, 
+    fileSize: 1024 * 1024
 };
-let         upload = multer({ storage: storage, limits: limits, fileFilter: fileFilter });
+let         upload = multer({ storage: storage, fileFilter: fileFilter });
 const         PORT = process.env.IMAGE_SERVER_PORT || 4005;
 const          app = express();
 
@@ -39,30 +38,35 @@ app.get('/image/server/change-user-image', (req, res) => {
     res.redirect('/auth/server/edit-profile');
 });
 app.post('/image/server/change-user-image', upload.single('userAvatar'), async (req, res) => {
-    console.log(`!!!!!!!!!!! image server, req.body= ${JSON.stringify(req.body)}}`);
-    
-    const imageDir = '/upload/';
-    await setUserImage(JSON.parse(req.cookies.userLoginInfo).email, req.file.filename, imageDir).catch((e) => console.log(e) );
-    
-    const changeUserName = (newName) => { JSON.parse(req.cookies.userLoginInfo).name = newName };
-    // pass the setting of new user name and password to the auth server
-    const URL = `${req.protocol}://${req.headers.host}/auth/server/edit-profile`;
-    axios({
-            method: 'POST',
-            url: URL,
-            data: {
-                userLoginInfo: req.cookies.userLoginInfo,
-                updatedUserName: req.body.userName,
-                updatedUserPassword: req.body.newPassword
-            }
-            })
-            .then((result) => result.data)
-            .then((data) => {
-                console.log(`!!!!!!!!!!! update change-user-image route successfully`);
-                changeUserName(req.body.userName);
-                res.redirect(`${req.protocol}://${req.headers.host}/auth/server/edit-profile`);
-            })
-            .catch((err) => res.send(`Error ${err}`));
+    try {
+        console.log(`!!!!!!!!!!! image server, req.body= ${JSON.stringify(req.body)}}, req.file= ${req.file}`);
+        if (req.file) {
+            const imageDir = '/upload/';
+            await setUserImage(JSON.parse(req.cookies.userLoginInfo).email, req.file.filename, imageDir).catch((e) => console.log(e) );
+        }
+        
+        const changeUserName = (newName) => { JSON.parse(req.cookies.userLoginInfo).name = newName };
+        // pass the setting of new user name and password to the auth server
+        const URL = `${req.protocol}://${req.headers.host}/auth/server/edit-profile`;
+        axios({
+                method: 'POST',
+                url: URL,
+                data: {
+                    userLoginInfo: req.cookies.userLoginInfo,
+                    updatedUserName: req.body.userName,
+                    updatedUserPassword: req.body.newPassword
+                }
+                })
+                .then((result) => result.data)
+                .then((data) => {
+                    console.log(`!!!!!!!!!!! update change-user-image route successfully`);
+                    changeUserName(req.body.userName);
+                    res.redirect(`${req.protocol}://${req.headers.host}/auth/server/edit-profile`);
+                })
+                .catch((err) => res.send(`Error ${err}`));
+    } catch (error) {
+        console.log(`Error in change user image route, ${error}`);
+    }
 });
 
 
