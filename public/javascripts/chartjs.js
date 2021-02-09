910caf991830e5ec3518a0fe26e8da4d8536e39a
LuanLuu <lluu3@mail.sfsu.edu>
2021-02-09 11:22:27 -0800

    Fix javascript dropdown menu in chart page

--- public/javascripts/chartjs.js
+++ public/javascripts/chartjs.js
@@ -1,3 +1,39 @@
+// ------- dropdown menu hover ---------
+let MIN_WIDTH      = 851;
+let dropdown       = undefined;
+let dropdownToggle = undefined;
+let dropdownMenu   = undefined;
+const inCallback   = () => { // the case where the dropdown menu is hovered
+  dropdown.addClass("show");
+  dropdownToggle.attr("aria-expanded", "true");
+  dropdownMenu.addClass("show");
+};
+const outCallback = () => { // the case where the dropdown menu is not hovered
+  dropdown.removeClass("show");
+  dropdownToggle.attr("aria-expanded", "false");
+  dropdownMenu.removeClass("show");
+};
+const getDropdownComponents = () => {
+  dropdown       = $(".dropdown");
+  dropdownToggle = $(".dropdown-toggle");
+  dropdownMenu   = $(".dropdown-menu");
+}
+const modifyDropdownProps = () => {
+  // if the window size is greater than MIN_WIDTH then add some new classes & attrs for the dropdown components
+  if (window.matchMedia("(min-width: " + MIN_WIDTH + "px)").matches) {
+    // check if components fail to load
+    if (dropdown === undefined || dropdownToggle === undefined || dropdownMenu === undefined) getDropdownComponents();
+    
+    dropdown.hover(inCallback, outCallback);
+    return;
+  } 
+  // otherwise, remove the above added classes & attrs for the dropdown components
+  dropdown.off("mouseenter mouseleave");
+};
+window.onresize = () => { 
+  modifyDropdownProps();
+}; 
+
 const renderChart = (data, labels) => {
     console.log(`!!!!! renderChart is called, labels= ${labels}, data= ${JSON.stringify(data)}, data.length= ${data.length}`);
     let ctx = document.getElementById("myChart").getContext('2d');
@@ -119,4 +155,5 @@ const getChartData = () => {
 
 window.onload = () => {
     getChartData();
+    modifyDropdownProps();
 };
