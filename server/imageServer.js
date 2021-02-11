a2a92623791632de9f473b80cad08f0b0dceed36
LuanLuu <lluu3@mail.sfsu.edu>
2021-02-10 16:04:22 -0800

    Reduce uploaded image size added

--- server/imageServer.js
+++ server/imageServer.js
@@ -5,14 +5,14 @@ const { setUserImage } = require('../database/users');
 const       morgan = require('morgan');
 const        axios = require('axios');
 const cookieParser = require('cookie-parser');
-const { PROTOCOL } = require('../utils/utils');
+const { PROTOCOL, currentYear } = require('../utils/utils');
+const         Jimp = require('jimp');
 const       multer = require('multer');
 const      storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(__dirname, '..', 'public', 'upload'));
     },
     filename: (req, file, cb) => {
-        console.log(file);
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
@@ -35,18 +35,45 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 
+(process.env.PRODUCTION === 'NO') ? app.set('views', 'views') : app.set('views', '../views');
+app.set('view engine', 'pug');
+
 app.get('/image/server/change-user-image', (req, res) => {
     res.redirect('/auth/server/edit-profile');
 });
-app.post('/image/server/change-user-image', upload.single('userAvatar'), async (req, res) => {
+const multerMiddleWare = (req, res, next) => {
+    const uploadHandler = upload.single('userAvatar');
+    uploadHandler(req, res, (error) => {
+        if (error) { //instanceof multer.MulterError
+            const userInCookie = JSON.parse(req.cookies.userLoginInfo);
+            if (error.code == 'LIMIT_FILE_SIZE') {
+                error.message = 'File Size is too large. Allowed max file size is 1MB :)';
+                return res.render('pages/editProfile', { year: currentYear, actionType: 'Upload file', status: 'fail', code: '500', error: error.message, user: userInCookie, isLoggedin: true  });
+            }
+            return res.render('pages/editProfile', { year: currentYear, actionType: 'Upload file', status: 'fail', code: '500', error: `Multer file upload error: ${err}`, user: userInCookie, isLoggedin: true  });
+        } 
+        next();
+    });
+};
+app.post('/image/server/change-user-image', multerMiddleWare, async (req, res) => {
     try {
-        console.log(`!!!!!!!!!!! image server, req.body= ${JSON.stringify(req.body)}}, req.file= ${req.file}`);
+        // console.log(`!!!!!!!!!!! image server, req.body= ${JSON.stringify(req.body)}}, req.file= ${req.file}, req.file.filename= ${req.file.filename}, req.file.filepath= ${req.file.filepath}`);
         let newImageLink = '';
         if (req.file) {
             const imageDir = '/upload/';
             await setUserImage(JSON.parse(req.cookies.userLoginInfo).email, req.file.filename, imageDir).catch((e) => console.log(e) );
             // update user cookie image
             newImageLink = `${imageDir}${req.file.filename}`;
+            // reduce image size
+            const filepath = `./public/upload/${req.file.filename}`;
+            Jimp.read(filepath)
+                .then((image) => {
+                    image
+                        .resize(150, 150)
+                        .quality(60)
+                        .write(filepath);
+                })
+                .catch((e) => { console.log(`Error in jimp reading file, ${e}`) });
         }
         
         const changeUserName = (newName) => { 
